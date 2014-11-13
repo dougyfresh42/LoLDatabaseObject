71255d0f155a2304ad1b2b5395c6e6eb8edca7a3
schizo529 <schizo.eric@gmail.com>
2014-11-13 13:17:41 -0500

    Added Summoner Spell tab

---
+++ SummonerSpellPanel.java
@@ -0,0 +1,45 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+
+public class SummonerSpellPanel extends JPanel {
+
+	private JTable table;
+	private DefaultTableModel tableModel = new DefaultTableModel();
+	
+	/**
+	 * Create the panel.
+	 */
+	public SummonerSpellPanel() 
+	{
+		this.setLayout(new BorderLayout());
+		
+		tableModel.setColumnCount(2);
+		
+		tableModel.addRow(new String[]{"Name:"});
+		tableModel.addRow(new String[]{"Description:"});
+		
+		table = new JTable(tableModel)
+		{
+		      public boolean isCellEditable(int row, int column){  
+		          return false;  
+		      }
+		};
+		
+		table.setShowGrid(false);
+		table.setShowHorizontalLines(false);
+		table.setShowVerticalLines(false);
+		table.setCellSelectionEnabled(false);
+		
+		table.getColumnModel().getColumn(0).setPreferredWidth(120);
+		table.getColumnModel().getColumn(1).setPreferredWidth(120);
+		
+		table.setRowHeight(1, 40);
+		
+		this.add(table, BorderLayout.WEST);
+	}
+
+}
