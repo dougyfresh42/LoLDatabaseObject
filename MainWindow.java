8d7a423cb02637fc63d2274e915116f10da3e9cf
schizo529 <schizo.eric@gmail.com>
2014-11-14 13:05:44 -0500

    Tab panes now display list of appropriate data

--- MainWindow.java
+++ MainWindow.java
@@ -11,6 +11,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.JList;
 
 import java.awt.ScrollPane;
+import java.sql.ResultSet;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
@@ -20,6 +21,8 @@ import javax.swing.JLabel;
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
+	
+	private SQLHelper sql;
 	
 	private DefaultListModel<String> championsModel = new DefaultListModel<String>();
 	private DefaultListModel<String> itemsModel = new DefaultListModel<String>();
@@ -47,85 +50,104 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 650, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		contentPane.add(tabbedPane, BorderLayout.CENTER);
-		
-		// Champion Tab
-		JSplitPane championPane = new JSplitPane();
-		championPane.setResizeWeight(.25);
-		tabbedPane.addTab("Champions", null, championPane, null);
-		
-		championsModel.addElement("One");
-		championsModel.addElement("Two");
-		championsModel.addElement("Three");
-		
-		JList<String> championNameList = new JList<String>(championsModel);
-		championPane.setLeftComponent(championNameList);		
-		
-		championPane.setRightComponent(new ChampionPanel());		
-		
-		// Item Tab
-		JSplitPane itemPane = new JSplitPane();
-		itemPane.setResizeWeight(.25);
-		tabbedPane.addTab("Items", null, itemPane, null);
-		
-		itemsModel.addElement("One");
-		itemsModel.addElement("Two");
-		itemsModel.addElement("Three");
-		
-		JList<String> itemNameList = new JList<String>(itemsModel);
-		itemPane.setLeftComponent(itemNameList);
-		
-		itemPane.setRightComponent(new ItemPanel());
-		
-		// Masteries Tab
-		JSplitPane masteriesPane = new JSplitPane();
-		masteriesPane.setResizeWeight(.25);
-		tabbedPane.addTab("Masteries", null, masteriesPane, null);
-		
-		masteriesModel.addElement("One");
-		masteriesModel.addElement("Two");
-		masteriesModel.addElement("Three");
-		
-		JList<String> masteryNameList = new JList<String>(masteriesModel);
-		masteriesPane.setLeftComponent(masteryNameList);
-		
-		masteriesPane.setRightComponent(new MasteryPanel());
-		
-		// Runes Tab
-		JSplitPane runePane = new JSplitPane();
-		runePane.setResizeWeight(.25);
-		tabbedPane.addTab("Rune", null, runePane, null);
-		
-		runesModel.addElement("One");
-		runesModel.addElement("Two");
-		runesModel.addElement("Three");
-		
-		JList<String> runeNameList = new JList<String>(runesModel);
-		runePane.setLeftComponent(runeNameList);
-		
-		runePane.setRightComponent(new RunePanel());
-		
-		// Summoner Spells Tab
-		JSplitPane summonerSpellsPane = new JSplitPane();
-		summonerSpellsPane.setResizeWeight(.25);
-		tabbedPane.addTab("Summoner Spells", null, summonerSpellsPane, null);
-		
-		summonerSpellsModel.addElement("One");
-		summonerSpellsModel.addElement("Two");
-		summonerSpellsModel.addElement("Three");
-		
-		JList<String> summonerSpellsNameList = new JList<String>(summonerSpellsModel);
-		summonerSpellsPane.setLeftComponent(summonerSpellsNameList);
-		
-		summonerSpellsPane.setRightComponent(new SummonerSpellPanel());
+		try
+		{
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, 650, 500);
+			contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			contentPane.setLayout(new BorderLayout(0, 0));
+			setContentPane(contentPane);
+			
+			sql = new SQLHelper();
+			
+			JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+			contentPane.add(tabbedPane, BorderLayout.CENTER);
+			
+			// Champion Tab
+			JSplitPane championPane = new JSplitPane();
+			championPane.setResizeWeight(.25);
+			tabbedPane.addTab("Champions", null, championPane, null);
+			
+			ResultSet champs = sql.selectAllChampion();
+			while(champs.next())
+			{
+				championsModel.addElement(champs.getString("ChampName"));
+			}
+			
+			JList<String> championNameList = new JList<String>(championsModel);
+			championPane.setLeftComponent(championNameList);		
+			
+			championPane.setRightComponent(new ChampionPanel());		
+			
+			// Item Tab
+			JSplitPane itemPane = new JSplitPane();
+			itemPane.setResizeWeight(.25);
+			tabbedPane.addTab("Items", null, itemPane, null);
+			
+			ResultSet items = sql.selectAllItem();
+			while(items.next())
+			{
+				itemsModel.addElement(items.getString("ItemName"));
+			}
+			
+			JList<String> itemNameList = new JList<String>(itemsModel);
+			itemPane.setLeftComponent(itemNameList);
+			
+			itemPane.setRightComponent(new ItemPanel());
+			
+			// Masteries Tab
+			JSplitPane masteriesPane = new JSplitPane();
+			masteriesPane.setResizeWeight(.25);
+			tabbedPane.addTab("Masteries", null, masteriesPane, null);
+			
+			ResultSet masteries = sql.selectAllMastery();
+			while(masteries.next())
+			{
+				masteriesModel.addElement(masteries.getString("MasteryName"));
+			}
+			
+			JList<String> masteryNameList = new JList<String>(masteriesModel);
+			masteriesPane.setLeftComponent(masteryNameList);
+			
+			masteriesPane.setRightComponent(new MasteryPanel());
+			
+			// Runes Tab
+			JSplitPane runePane = new JSplitPane();
+			runePane.setResizeWeight(.25);
+			tabbedPane.addTab("Rune", null, runePane, null);
+			
+			ResultSet runes = sql.selectAllRune();
+			while(runes.next())
+			{
+				runesModel.addElement(runes.getString("RuneName"));
+			}
+			
+			JList<String> runeNameList = new JList<String>(runesModel);
+			runePane.setLeftComponent(runeNameList);
+			
+			runePane.setRightComponent(new RunePanel());
+			
+			// Summoner Spells Tab
+			JSplitPane summonerSpellsPane = new JSplitPane();
+			summonerSpellsPane.setResizeWeight(.25);
+			tabbedPane.addTab("Summoner Spells", null, summonerSpellsPane, null);
+			
+			ResultSet summonerSpells = sql.selectAllSummonerSpell();
+			while(summonerSpells.next())
+			{
+				summonerSpellsModel.addElement(summonerSpells.getString("SummonerSpellName"));
+			}
+			
+			JList<String> summonerSpellsNameList = new JList<String>(summonerSpellsModel);
+			summonerSpellsPane.setLeftComponent(summonerSpellsNameList);
+			
+			summonerSpellsPane.setRightComponent(new SummonerSpellPanel());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
